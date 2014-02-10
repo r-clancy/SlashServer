29e2821887e8fe9c096c4533b844b571acaca1e1
rylinaux <rylinaux@gmail.com>
2014-02-09 22:48:18 -0400

    Remove unused code

--- src/main/java/com/rylinaux/slashserver/SlashServer.java
+++ src/main/java/com/rylinaux/slashserver/SlashServer.java
@@ -2,34 +2,19 @@ package com.rylinaux.slashserver;
 
 import com.rylinaux.slashserver.commands.SlashServerCommand;
 
-import lombok.Getter;
-
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class SlashServer extends Plugin {
 
-    @Getter
-    private static SlashServer instance;
-
     @Override
     public void onEnable() {
-        instance = this;
-        loadCommands();
-    }
-
-    @Override
-    public void onDisable() {
-        unloadCommands();
-        instance = null;
-    }
-
-    public void loadCommands() {
         for (String server : this.getProxy().getServers().keySet()) {
             this.getProxy().getPluginManager().registerCommand(this, new SlashServerCommand(server, String.format("ss.%s", server)));
         }
     }
 
-    public void unloadCommands() {
+    @Override
+    public void onDisable() {
         this.getProxy().getPluginManager().unregisterCommands(this);
     }
 
